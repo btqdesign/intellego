661f9a65d9ccac6f9083e3f78e499a7a085c74c1
ivan9146 <ivan9146@gmail.com>
2015-05-07 14:17:21 -0500

    Pruebas Plugin
    Pruebas Plugin

--- wp-content/plugins/threewp-broadcast/src/traits/post_actions.php
+++ wp-content/plugins/threewp-broadcast/src/traits/post_actions.php
@@ -26,10 +26,11 @@ trait post_actions
 				$this->add_filter( 'manage_posts_columns' );
 				$this->add_filter( 'manage_pages_columns', 'manage_posts_columns' );
 
-				$this->add_filter( 'manage_project_posts_columns', 'manage_posts_custom_column' );
-
 				$this->add_action( 'manage_posts_custom_column', 10, 2 );
 				$this->add_action( 'manage_pages_custom_column', 'manage_posts_custom_column', 10, 2 );
+
+				$post_type = isset( $_REQUEST[ 'post_type' ] ) ? $_REQUEST[ 'post_type' ] : 'post';
+				echo '*********??*******'.$post_type;
 			}
 
 			// Hook into the actions so that we can keep track of the broadcast data.
