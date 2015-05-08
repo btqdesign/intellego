36cc96b2a3d7e5d9a672c5e9334258a750bb0fb8
ivan9146 <ivan9146@gmail.com>
2015-05-08 09:26:42 -0500

    Pruebas Plugin

--- wp-content/themes/lassic-theme/include/theme-components/cs-widgets/widgets.php
+++ wp-content/themes/lassic-theme/include/theme-components/cs-widgets/widgets.php
@@ -712,9 +712,9 @@ if ( ! class_exists( 'recentpostsproj' ) ) {
 				 *
 				 */
 				if(isset($select_category) and $select_category <> ' ' and $select_category <> ''){
-					$args = array( 'posts_per_page' => "$showcount",'post_type' => 'project','category_name' => "$select_category", 'ignore_sticky_posts' => 1);
+					$args = array( 'posts_per_page' => "$showcount",'post_type' => 'project','ignore_sticky_posts' => 1);
 				}else{
-					$args = array( 'posts_per_page' => "$showcount",'post_type' => 'project', 'ignore_sticky_posts' => 1);
+					$args = array( 'posts_per_page' => "$showcount",'post_type' => 'project','ignore_sticky_posts' => 1);
 				}
 
 			  $custom_query = new WP_Query($args);
