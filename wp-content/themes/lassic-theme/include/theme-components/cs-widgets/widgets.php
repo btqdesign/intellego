3ae5cebfd12e79f9b39d0df9404e5601b4ab5979
ivan9146 <ivan9146@gmail.com>
2015-05-28 10:22:19 -0500

    Mejoras Widget

--- wp-content/themes/lassic-theme/include/theme-components/cs-widgets/widgets.php
+++ wp-content/themes/lassic-theme/include/theme-components/cs-widgets/widgets.php
@@ -1040,15 +1040,28 @@ if ( ! class_exists( 'relatedposts' ) ) {
 				  }
 				endwhile; 
 				 if($thumb <> true) echo '</ul>';
-                  }
-                  else {
-					  ?>No hay resultados<?php
-                      if ( function_exists( 'cs_no_result_found' ) ) { cs_no_result_found(false); }
+			} else {
+				
+					  
+				$args = array(
+					'posts_per_page' => "$showcount",
+					'post_type' => "$typo_post",
+					'post_status' => 'publish',
+					'orderby' => 'rand',
+					'post__not_in' => array ($id_post_id),
+				);	
+				$custom_query = new WP_Query($args);  
+				  
+				if ( $custom_query->have_posts() <> "" ) {
+					?>Aleatorio<?
+				}else{
+					if ( function_exists( 'cs_no_result_found' ) ) { cs_no_result_found(false); }
+				}
 
-                  }
-			    echo cs_allow_special_char($after_widget);
-			  }
-		  }
+			}
+			echo cs_allow_special_char($after_widget);
+		}
+	}
 }
 add_action( 'widgets_init', create_function('', 'return register_widget("relatedposts");') );
 
