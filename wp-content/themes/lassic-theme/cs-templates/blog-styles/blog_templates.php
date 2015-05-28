640539c0db194b1b2f8de939694a4ddb5d0ef04c
ivan9146 <ivan9146@gmail.com>
2015-05-28 12:44:02 -0500

    Pruebas Fecha

--- wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
+++ wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
@@ -64,8 +64,8 @@ if ( !class_exists('cs_blog_templates') ) {
                                 <ul class="cs-post-options">
                                 	<?php cs_featured(); ?>
                                     <!--<li><i class="icon-user9"></i><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a></li>-->
-                                    <li><i class=" icon-calendar11"></i><time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',$post->ID)));?>">
-                                        <?php echo date_i18n('F d,Y',strtotime(get_the_date('d/m/Y',$post->ID)));?></time>
+                                    <li><i class=" icon-calendar11"></i><time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',get_the_id())));?>">
+                                        <?php echo date_i18n('F d,Y',strtotime(get_the_date('d/m/Y',get_the_id())));?></time>
                                     </li>
                                     <li><i class=" icon-comment2"></i>
                                         <span><?php echo comments_number(__('0', 'lassic'), __('1', 'lassic'), __('%', 'lassic') );?></span>
@@ -130,8 +130,8 @@ if ( !class_exists('cs_blog_templates') ) {
                                	<ul class="cs-post-options">
                                 	<?php cs_featured(); ?>
 									<!--<li><i class="icon-user9"></i><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a></li>-->
-                                    <li><i class=" icon-calendar11"></i><time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',$post->ID)));?>">
-										<?php echo date_i18n('F d,Y',strtotime(get_the_date('d/m/Y',$post->ID)));?></time>
+                                    <li><i class=" icon-calendar11"></i><time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',get_the_id())));?>">
+										<?php echo date_i18n('F d,Y',strtotime(get_the_date('d/m/Y',get_the_id())));?></time>
                                     </li>
  									<li><i class=" icon-comment2"></i>
 										<span><?php echo comments_number(__('0', 'lassic'), __('1', 'lassic'), __('%', 'lassic') );?></span>
@@ -196,8 +196,8 @@ if ( !class_exists('cs_blog_templates') ) {
                                     	</a>
                                     </li>                               
                                     -->
-                                    <li><i class=" icon-calendar11"></i><time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',$post->ID)));?>">
-                                        <?php echo date_i18n('F d,Y',strtotime(get_the_date('d/m/Y',$post->ID)));?></time>
+                                    <li><i class=" icon-calendar11"></i><time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',get_the_id())));?>">
+                                        <?php echo date_i18n('F d,Y',strtotime(get_the_date('d/m/Y',get_the_id())));?></time>
                                     </li>
 								</ul>
  							</div>
@@ -260,8 +260,8 @@ if ( !class_exists('cs_blog_templates') ) {
                                     	</a>
                                     </li>                               
                                     -->
-                                    <li><i class=" icon-calendar11"></i><time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',$post->ID)));?>">
-                                        <?php echo date_i18n('F d,Y',strtotime(get_the_date('d/m/Y',$post->ID)));?></time>
+                                    <li><i class=" icon-calendar11"></i><time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',get_the_id())));?>">
+                                        <?php echo date_i18n('F d,Y',strtotime(get_the_date('d/m/Y',get_the_id())));?></time>
                                     </li>
 								</ul>
  							</div>
