1f5ae8c2ef9d76e22c0b7b118748cef8660ee0a0
ivan9146 <ivan9146@gmail.com>
2015-05-28 12:59:37 -0500

    Pruebas fechas
    Pruebas fechas

--- wp-content/themes/lassic-theme/single.php
+++ wp-content/themes/lassic-theme/single.php
@@ -176,7 +176,7 @@
                         </ul> 
                         <ul class="cs-post-options">
                             <!--<li><i class="icon-user9"></i><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a></li>-->
-                            <li><i class=" icon-calendar11"></i><time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',get_the_id())));?>">
+                            <li><i class=" icon-calendar11"></i>--<time datetime="<?php echo date_i18n('Y-m-d',strtotime(get_the_date('d/m/Y',get_the_id())));?>">
                                 <?php echo date_i18n('F d,Y',strtotime(get_the_date('d/m/Y',get_the_id())));?></time>
                             </li>
                             <li><i class=" icon-comment2"></i>
