2ecce014900617b81a46041aded112f0bfce27a9
ivan9146 <ivan9146@gmail.com>
2015-06-11 18:03:07 -0500

    Traductor
    Traductor

--- wp-content/themes/lassic-theme/searchform.php
+++ wp-content/themes/lassic-theme/searchform.php
@@ -7,7 +7,7 @@ global $cs_theme_options
 <div class="cs-search-area">
 <form method="get" action="<?php echo home_url()?>" role="search">
 	<i class="icon-search6"></i>
-    <input type="text" class="form-control" onfocus="if(this.value =='<?php  _e('Enter Title, Keyword, Company','lassic');?>') { this.value = ''; }" onblur="if(this.value == '') { this.value ='<?php  _e('Enter Title, Keyword, Company','lassic');?>'; }" value="<?php  _e('Enter Title, Keyword, Company','lassic');?>" name="s" id="s">
+    <input type="text" class="form-control" onfocus="if(this.value =='<?php  _e('Buscar, Company','lassic');?>') { this.value = ''; }" onblur="if(this.value == '') { this.value ='<?php  _e('Buscar','lassic');?>'; }" value="<?php  _e('Buscar','lassic');?>" name="s" id="s">
     <label>
         <input type="submit" class="btnsubmit cs-bg-color" value="Search">
     </label>
