ca2a4cd4aae12405c711158c969f2df8b17ab773
ivan9146 <ivan9146@gmail.com>
2015-06-12 08:35:31 -0500

    Leer más...

--- wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
+++ wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
@@ -248,7 +248,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   ?>
 					   	<div class="cs-bloginfo-sec">
 					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
-						  	<?php if ($description == 'yes') {?><p> <?php echo cs_get_the_excerpt($excerpt,'true','Rsead more');?></p><?php } ?> 	
+						  	<?php if ($description == 'yes') {?><p> <?php echo cs_get_the_excerpt($excerpt,'true','Leer mas...');?></p><?php } ?> 	
 							<div class="cs-blog-text">
 								<ul class="cs-post-options">
                                 	<?php cs_featured(); ?>
