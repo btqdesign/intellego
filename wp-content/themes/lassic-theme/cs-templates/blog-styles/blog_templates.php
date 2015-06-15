063c63e4a178c29ff45ded2812152634f05d551e
ivan9146 <ivan9146@gmail.com>
2015-06-15 16:55:20 -0500

    Traduccion

--- wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
+++ wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
@@ -49,7 +49,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   ?>
 					   <div class="cs-bloginfo-sec">
 							<h2><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h2>
-						  	<?php if ($description == 'yes') {?><p> <?php _e(cs_get_the_excerpt($excerpt,'true','Read more...'),'lassic'); ?></p><?php } ?> 	
+						  	<?php if ($description == 'yes') {?><p> <?php echo cs_get_the_excerpt($excerpt,'true','Read more...');?></p><?php } ?> 	
                             <ul class="cs-category">
                                 <?php  
                                     $categories_list = get_the_term_list ( get_the_id(), 'category', '', '', '' );
@@ -115,7 +115,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   ?>
 					   <div class="cs-bloginfo-sec">
 						  <h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
-						  <?php if ($description == 'yes') {?><p> <?php _e(cs_get_the_excerpt($excerpt,'true','Read more...'),'lassic'); ?></p><?php } ?> 	
+						  <?php if ($description == 'yes') {?><p> <?php echo cs_get_the_excerpt($excerpt,'true','Read more...');?></p><?php } ?> 	
 							<div class="cs-blog-text">
 								<ul class="cs-category">
 									<?php  
@@ -184,7 +184,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   ?>
 					   	<div class="cs-bloginfo-sec">
 					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
-						  	<?php if ($description == 'yes') {?><p> <?php _e(cs_get_the_excerpt($excerpt,'true','Read more...'),'lassic'); ?></p><?php } ?> 	
+						  	<?php if ($description == 'yes') {?><p> <?php echo cs_get_the_excerpt($excerpt,'true','Read more...');?></p><?php } ?> 	
 							<div class="cs-blog-text">
 								<ul class="cs-post-options">
                                 	<?php cs_featured();?>
@@ -248,7 +248,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   ?>
 					   	<div class="cs-bloginfo-sec">
 					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
-						  	<?php if ($description == 'yes') {?><p> <?php _e(cs_get_the_excerpt($excerpt,'true','Read more...'),'lassic'); ?></p><?php } ?> 	
+						  	<?php if ($description == 'yes') {?><p> <?php echo cs_get_the_excerpt($excerpt,'true','Read more...');?></p><?php } ?> 	
 							<div class="cs-blog-text">
 								<ul class="cs-post-options">
                                 	<?php cs_featured(); ?>
--- wp-content/themes/lassic-theme/searchform.php
+++ wp-content/themes/lassic-theme/searchform.php
@@ -7,7 +7,7 @@ global $cs_theme_options
 <div class="cs-search-area">
 <form method="get" action="<?php echo home_url()?>" role="search">
 	<i class="icon-search6"></i>
-    <input type="text" class="form-control" onfocus="if(this.value =='<?php  _e('Buscar','lassic');?>') { this.value = ''; }" onblur="if(this.value == '') { this.value ='<?php  _e('Buscar','lassic');?>'; }" value="<?php  _e('Buscar','lassic');?>" name="s" id="s">
+    <input type="text" class="form-control" onfocus="if(this.value =='<?php  _e('Search','lassic');?>') { this.value = ''; }" onblur="if(this.value == '') { this.value ='<?php  _e('Search','lassic');?>'; }" value="<?php  _e('Search','lassic');?>" name="s" id="s">
     <label>
         <input type="submit" class="btnsubmit cs-bg-color" value="Search">
     </label>
