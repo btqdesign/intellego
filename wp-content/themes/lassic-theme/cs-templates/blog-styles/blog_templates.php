753a7f8f7c174220acc2949223e5074cf4d47f1e
ivan9146 <ivan9146@gmail.com>
2015-06-15 17:27:27 -0500

    traduccion

--- wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
+++ wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
@@ -49,8 +49,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   ?>
 					   <div class="cs-bloginfo-sec">
 							<h2><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h2>
-							<?php $txt_more = _e('Read more...','lassic'); ?>
-						  	<?php if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', $txt_more);?></p><?php } ?> 	
+						  	<?php if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Read more...');?></p><?php } ?> 	
                             <ul class="cs-category">
                                 <?php  
                                     $categories_list = get_the_term_list ( get_the_id(), 'category', '', '', '' );
@@ -116,8 +115,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   ?>
 					   <div class="cs-bloginfo-sec">
 						  <h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
-						  <?php $txt_more = _e('Read more...','lassic'); ?>
-						  <?php if ($description == 'yes') {?><p> <?=echo cs_get_the_excerpt($excerpt,'true',$txt_more);?></p><?php } ?> 	
+						  <?php if ($description == 'yes') {?><p> <?=echo cs_get_the_excerpt($excerpt,'true','Read more...');?></p><?php } ?> 	
 							<div class="cs-blog-text">
 								<ul class="cs-category">
 									<?php  
@@ -186,8 +184,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   ?>
 					   	<div class="cs-bloginfo-sec">
 					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
-							<?php $txt_more = _e('Read more...','lassic'); ?>
-						  	<?php if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true',$txt_more);?></p><?php } ?> 	
+						  	<?php if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Read more...');?></p><?php } ?> 	
 							<div class="cs-blog-text">
 								<ul class="cs-post-options">
                                 	<?php cs_featured();?>
@@ -252,7 +249,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   	<div class="cs-bloginfo-sec">
 					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
 							<?php print_r(_e('Read more...','lassic')); ?>
-						  	<?php if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true',$txt_more);?></p><?php } ?> 	
+						  	<?php if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Read more...');?></p><?php } ?> 	
 							<div class="cs-blog-text">
 								<ul class="cs-post-options">
                                 	<?php cs_featured(); ?>
