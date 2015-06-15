e217155c0d3e8b3f7d5f868eec682ad8c91309ce
ivan9146 <ivan9146@gmail.com>
2015-06-15 18:29:10 -0500

    Traduccion

--- wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
+++ wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
@@ -49,6 +49,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   ?>
 					   <div class="cs-bloginfo-sec">
 							<h2><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h2>
+
 						  	<?php
 							if(substr(bloginfo ( 'language' ), 0, 2) == 'es-MX'){
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Leer Más...');?></p><?php }
@@ -267,7 +268,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   	<div class="cs-bloginfo-sec">
 					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
 						  	<?php
-							if(substr(bloginfo ( 'language' ), 0, 2) == 'es-MX'){
+							if(substr(bloginfo('language'), 0, 2) == 'es-MX'){
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Leer Más...');?></p><?php }
 							}else{
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Read more...');?></p><?php }
