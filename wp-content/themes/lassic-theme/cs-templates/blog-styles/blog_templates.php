8f1b90c2bf74138c2adb359b048018ac11bf5312
ivan9146 <ivan9146@gmail.com>
2015-06-16 09:01:51 -0500

    Traducir

--- wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
+++ wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
@@ -51,11 +51,6 @@ if ( !class_exists('cs_blog_templates') ) {
 							<h2><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h2>
 						  	<?php
 							if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Leer Más...'); ?></p><?php }
-							if(get_bloginfo('language') == 'es-MX'){
-								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Leer Más...'); ?></p><?php }
-							}else{
-								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Read more...'); ?></p><?php }
-							}
 							?>
                             <ul class="cs-category">
                                 <?php  
@@ -124,11 +119,6 @@ if ( !class_exists('cs_blog_templates') ) {
 						  <h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
 						  	<?php
 							if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Leer Más...'); ?></p><?php }
-							if(get_bloginfo('language') == 'es-MX'){
-								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Leer Más...'); ?></p><?php }
-							}else{
-								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Read more...'); ?></p><?php }
-							}
 							?>
 							<div class="cs-blog-text">
 								<ul class="cs-category">
@@ -200,11 +190,6 @@ if ( !class_exists('cs_blog_templates') ) {
 					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
 						  	<?php
 							if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Leer Más...'); ?></p><?php }
-							if(get_bloginfo('language') == 'es-MX'){
-								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Leer Más...'); ?></p><?php }
-							}else{
-								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Read more...'); ?></p><?php }
-							}
 							?>
 							<div class="cs-blog-text">
 								<ul class="cs-post-options">
@@ -268,14 +253,8 @@ if ( !class_exists('cs_blog_templates') ) {
 							
 					   ?>
 					   	<div class="cs-bloginfo-sec">
-					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
 						  	<?php
 							if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Leer Más...'); ?></p><?php }
-							if(get_bloginfo('language') == 'es-MX'){
-								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Leer Más...'); ?></p><?php }
-							}else{
-								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true','Read more...'); ?></p><?php }
-							}
 							?>
 							<div class="cs-blog-text">
 								<ul class="cs-post-options">
