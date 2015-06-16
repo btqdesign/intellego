f1ec9a694bc73d57467baefc0d4260efb4517030
ivan9146 <ivan9146@gmail.com>
2015-06-16 08:39:12 -0500

    Traduccion

--- wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
+++ wp-content/themes/lassic-theme/cs-templates/blog-styles/blog_templates.php
@@ -51,7 +51,7 @@ if ( !class_exists('cs_blog_templates') ) {
 							<h2><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h2>
 
 						  	<?php
-							if(substr(bloginfo ( 'language' ), 0, 2) == 'es-MX'){
+							if(bloginfo('language') == 'es-MX'){
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Leer Más...');?></p><?php }
 							}else{
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Read more...');?></p><?php }
@@ -123,7 +123,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   <div class="cs-bloginfo-sec">
 						  <h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
 						  	<?php
-							if(substr(bloginfo ( 'language' ), 0, 2) == 'es-MX'){
+							if(bloginfo('language') == 'es-MX'){
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Leer Más...');?></p><?php }
 							}else{
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Read more...');?></p><?php }
@@ -198,7 +198,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   	<div class="cs-bloginfo-sec">
 					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
 						  	<?php
-							if(substr(bloginfo ( 'language' ), 0, 2) == 'es-MX'){
+							if(bloginfo('language') == 'es-MX'){
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Leer Más...');?></p><?php }
 							}else{
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Read more...');?></p><?php }
@@ -268,7 +268,7 @@ if ( !class_exists('cs_blog_templates') ) {
 					   	<div class="cs-bloginfo-sec">
 					  		<h4><a href="<?php esc_url(the_permalink());?>"><?php cs_get_title($cs_title_limit); ?></a></h4>
 						  	<?php
-							if(substr(bloginfo('language'), 0, 2) == 'es-MX'){
+							if(bloginfo('language') == 'es-MX'){
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Leer Más...');?></p><?php }
 							}else{
 								if ($description == 'yes') {?><p> <?=cs_get_the_excerpt($excerpt,'true', 'Read more...');?></p><?php }
